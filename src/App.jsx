b7485644549caefcefced8af6c71f14016de27b4
Alvaro Miranda <profalvaromirandaosorio@email.com>
2026-03-10 07:18:00 -0600

    Se actualiza Home.CSS

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productos" element={<Productos />} />
-        <Route path="/Galeroa" element={<Galeria />} />
+        <Route path="/galeria" element={<Galeria />} />
       </Routes>
     </Router>
   );
